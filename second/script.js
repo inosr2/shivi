106c65dc5a20dfa6b0688e26e523cc59d82d1c6d
rohit kumar <msrohitkumar132@gmail.com>
2025-04-03 18:23:22 +0530

    shivi birthday

--- second/script.js
+++ second/script.js
@@ -1,8 +1,8 @@
 var radius = 240; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 180; // width of images (unit: px)
-var imgHeight = 330; // height of images (unit: px)
+var imgWidth = 200; // width of images (unit: px)
+var imgHeight = 350; // height of images (unit: px)
 // Link of background music - set 'null' if you dont want to play background music
 var bgMusicURL = './music/love.mp3'; // Link of background music
 var bgMusicControls = true; // Show UI music control
