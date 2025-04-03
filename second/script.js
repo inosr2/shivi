bad8a0bd818afa6f5b502b31e9cf2605f7143364
rohit kumar <msrohitkumar132@gmail.com>
2025-04-03 20:28:28 +0530

    shivi birthday

--- second/script.js
+++ second/script.js
@@ -1,4 +1,4 @@
-var radius = 310; // how big of the radius
+var radius = 400; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
 var imgWidth = 300; // width of images (unit: px)
